dc2071d140f09582db338bed1f65c0dfbd83cd35
ErrorWinters <fnfsonic.111@gmail.com>
2026-01-15 16:59:10 -0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -103,13 +103,13 @@ export default {
                         Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record on the level that is listed on the site - please check the level ID and name before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have either source audio or clicks/taps in the video. Edited audio only does not count. Contact me if you have another way
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must have a previous attempt shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
                     </p>
                     <p>
                         The recording must also show the player hit the endwall, or the completion will be invalidated.
